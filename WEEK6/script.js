c0addb4dda94c22ae752a545ea01ac7c4b772cda
Musasizi Kenneth <kennymusasizi@hotmail.com>
2024-02-15 10:01:35 +0300

    Variables in Javascript

---
+++ WEEK6/script.js
@@ -0,0 +1,30 @@
+// console.log("Hello, world!");
+// VARIABLES
+let firstName = "Jane", age = 40, country = "UGANDA";
+
+//Declaration of variables
+let x, y, z;
+
+// Initialization of variables
+
+/*
+
+This is a multiline comment
+
+*/
+x = 20;
+y = 90;
+z = 4;
+
+// Updating variables
+x = 70;
+y = 10000;
+z = "off-limit"
+
+
+// var age = 20
+// var age = 20888
+
+const PIE = 3.43444
+
+console.log(70 + 10000)
